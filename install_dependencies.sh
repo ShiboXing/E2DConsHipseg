9b61c0534f341bab658a3cbdef05c06dc6c02f29
user.name <shx26@pitt.edu>
2019-12-17 09:15:48 +0800

    changed install_dependencies.sh

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+# need python2 and 3
+sudo apt install python3
+sudo apt install python 
+sudo apt install python3-pip
+sudo apt install python3-opencv
+sudo -H pip3 install torch torchvision matplotlib nibabel numpy scikit-image scipy pillow 
+sudo python fslinstaller.py
+
+
