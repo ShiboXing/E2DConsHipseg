fcb8da2b6f70cde83cb66ce8199647e9d0e8f356
--- <shx26@pitt.edu>
2020-01-08 09:04:32 -0500

    added install shell file for colab

---
+++ notebook_install_dependencies.sh
@@ -0,0 +1,13 @@
+# need python2 and 3
+!sudo apt install python3
+!sudo apt install python 
+!sudo apt install python3-pip
+!sudo apt install python3-opencv
+!sudo apt install python3-tk
+!sudo apt-get install -y python3-sparse  
+!sudo -H pip3 install torch torchvision matplotlib nibabel numpy scikit-image scipy pillow 
+!sudo python2 fslinstaller.py
+# to use matplotlib and tkinter, must install Xming on windows if ubuntu is a sub system on windows
+
+
+
